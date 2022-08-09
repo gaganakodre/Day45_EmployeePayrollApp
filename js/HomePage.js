20cf94b6607e53f52cbdd2698f354bd90725655d
shreegowri <gaganandh@gmail.com>
2022-08-09 23:02:03 +0530

    [shreegowri]Ability to Remove an Employee from the Payroll details.

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,25 +1,44 @@
-
+let empPayrollList;
 window.addEventListener("DOMContentLoaded", (event) => {
-    createInnerHtml(); });
+    empPayrollList=getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent=empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+ });
+ const getEmployeePayrollDataFromStorage=()=>{
+    return localStorage.getItem('EmployeePayrollList') ?
+    JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+ }
     const createInnerHtml=()=>{
-        const headerHtml= " <tr><th></th<th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-       let empPayRollData=createEmployeeJSON()[0]; 
-       const innerHtml=`${headerHtml}
-        <tr>
-            <td><img class="profile" src="${empPayRollData._profilPic}" alt=""></td>
+        const headerHtml= " <tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+       if(empPayrollList.length==0) return;
+        let innerHtml=`${headerHtml}`;
+        for(const empPayRollData of empPayrollList)
+        {console.log(empPayRollData)
+         innerHtml=`${innerHtml}
+            <tr>
+            <td><img src="${empPayRollData._profilePic}" alt="profile" style="width=20px;height=20px;"></td>
             <td>${empPayRollData._name}</td>
             <td>${empPayRollData._gender}</td>
-            <td><div class="dept-label">${empPayRollData._department[0]}</div><div class="dept-label">${empPayRollData._department[1]}</div></td>
+            <td>${getDeptHtml(empPayRollData._department)}</td>
             <td>${empPayRollData._salary}</td>
-            <td>${empPayRollData._startDate}</td>
+            <td>${stringifyDate(empPayRollData.date)}</td>
             <td>
-                <img name="${empPayRollData._id} " onclick="remove(this)" src="../assest/Icon/deleteIcon.svg" alt="delete" />
-                <img name="${empPayRollData._id} " onclick="update(this)" src="../assest/Icon/EditIcon.svg" alt="edit" />
+                <img name="${empPayRollData._name}" onclick="remove(this)" src="../assest/Icon/deleteIcon.svg" alt="delete" />
+                <img name="${empPayRollData._name}" onclick="update(this)" src="../assest/Icon/EditIcon.svg" alt="edit" />
             </td>
-        </tr>`;
+        </tr>`
+        };
         document.getElementById('display_container').innerHTML=innerHtml;
         
     }
+    const stringifyDate = (date) =>
+        {
+            const options = {day:'numeric', month:'short',year:'numeric'};
+            const newDate = !date?"undefined": new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
+            return newDate;
+
+        }
     const getDeptHtml=(deptList)=>{
      let deptHtml='';
      for(const dept of deptList){
@@ -37,7 +56,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 _startDate:'29 oct 2021',
                 _note:'',
                 _id:new Date().getTime(),
-                _profilePic:'../assets/profilepic/Ellipse-1.png'
+                _profilePic:'G:/FellowShip517/Day45_EmployeePayrollApp/Day45_EmployeePayrollApp/assest/profilepic/Ellipse -1.png'
             },
             {
                 _name:'karan',
@@ -52,6 +71,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
         ];
         return empPayrollListLocal;
     }
-    
+    const remove=(node)=>{
+        let empPayRollData=empPayrollList.find(empData=>empData._name==node.name);
+        if(!empPayRollData) return;
+        const index=empPayrollList
+        .map(empData=>empData._name)
+        .indexOf(empPayRollData._name);
+    empPayrollList.splice(index,1);
+    localStorage.setItem("EmplyeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent=empPayrollList.length;
+    createInnerHtml();
+    }
      
     
--- js/RegistrationForm.js
+++ js/RegistrationForm.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
     const salary = document.querySelector('#salary');
-    const output = document.querySelector('salary-output');
+    const output = document.querySelector('.salary-output');
     output.textContent= salary.value;
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
