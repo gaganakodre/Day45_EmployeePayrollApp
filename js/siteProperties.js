930b0a76d39a7cc3b47803f6ed204feb71fa1824
shreegowri <gaganandh@gmail.com>
2022-08-12 22:03:52 +0530

    [shreegowri]delete and update operation

--- js/siteProperties.js
+++ js/siteProperties.js
@@ -1,5 +1,5 @@
 let site_properties={
-    use_local_storage:"false",
+    use_local_storage:"true",
     home_page: "../pages/HomePage.html",
     add_emp_payroll_pages:"../pages/RegistrationForm.html",
     server_url:"http://localhost:3000/EmployeePayrollDB/"
